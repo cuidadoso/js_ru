8fa42fe582ab51d809b8ce9da44060d42d792086
Alexander <alexander_s@front.ru>
2018-05-10 01:12:28 +0300

    Small refactoring

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Article from './Article';
+import {Article} from './';
 
 export default function ArticleList({articles}){
     const articleElements = articles.map(article => <li key={article.id}><Article article = {article}/></li>);
