4511aa00e1eab40932830d0acb9f862b99ad809d
Alexander <alexander_s@front.ru>
2018-05-10 00:55:04 +0300

    Add ArticleList component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Article from './Article';
+import {ArticleList} from './components';
 import {articles} from './data';
 
 function HelloWorld() {
     return <h1>Hello world</h1>
 }
 
-ReactDOM.render(<Article article = { articles[0]} />, document.getElementById('root'));
+ReactDOM.render(<ArticleList articles = { articles} />, document.getElementById('root'));
 // registerServiceWorker();
