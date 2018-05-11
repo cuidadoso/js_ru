40d03dc245d66203c618bc2638aa1e750698bac0
Alexander Pyreev <apyreev@at-consulting.ru>
2018-05-11 17:26:41 +0300

    Rewrite CommentList to function

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,36 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import {Comment} from './';
 import {toggleOpen} from '../decorators';
 
-class CommentList extends Component {
-    static defaultProps = {
-        comments: []
-    };
-
-    getBody = () => {
-        const {comments, isOpen} = this.props;
-        if(!isOpen) return null;
-        if(!comments.length) {
-            return <p>No comments yet</p>;
-        }
-        return <ul>
-            {comments.map(comment => <li key={comment.id}><Comment comment = {comment}/></li>)}
-        </ul>;
-    };
-
-    render() {
-        const {isOpen, toggleOpen} = this.props;
+function CommentList ({comments = [], isOpen, toggleOpen}) {
         const buttonText = isOpen ? 'hide comments' : 'show comments';
         return (
             <div>
                 <button onClick={toggleOpen}>
                     {buttonText}
                 </button>
-                {this.getBody()}
+                {getBody({comments, isOpen})}
             </div>
         );
+}
+
+function getBody({comments, isOpen}) {
+    if(!isOpen) return null;
+    if(!comments.length) {
+        return <p>No comments yet</p>;
     }
+    return <ul>
+        {comments.map(comment => <li key={comment.id}><Comment comment = {comment}/></li>)}
+    </ul>;
 }
 
 export default toggleOpen(CommentList);
