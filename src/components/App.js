7430fd05450fddc245f00a47e82985ac4a819dab
Alexander Pyreev <apyreev@at-consulting.ru>
2018-05-23 14:07:21 +0300

    Add simple routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import { ArticleList, Counter, Filters, UserForm } from './';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Counter />
-        <UserForm />
-        <Filters />
-        <ArticleList />
-      </div>
+      <Router>
+        <div>
+          <div>
+            <h2>Main menu</h2>
+            <div>
+              <NavLink activeStyle={{ color: 'red' }} to="/counter">
+                Counter
+              </NavLink>
+            </div>
+            <div>
+              <NavLink activeStyle={{ color: 'red' }} to="/filters">
+                Filters
+              </NavLink>
+            </div>
+            <div>
+              <NavLink activeStyle={{ color: 'red' }} to="/articles">
+                Articles
+              </NavLink>
+            </div>
+          </div>
+          <UserForm />
+          <Route path="/counter" component={Counter} />
+          <Route path="/filters" component={Filters} />
+          <Route path="/articles" component={ArticleList} />
+        </div>
+      </Router>
     );
   }
 }
